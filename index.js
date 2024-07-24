3ab1d954a5ebdecb5586d86d936cb265aa2678fb
unknown <akshatsomvanshi22@gmail.com>
2024-07-24 15:46:23 +0530

    initial commit

--- index.js
+++ index.js
@@ -5,54 +5,88 @@ const {
   clusterApiUrl,
   Keypair,
   LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
 } = require("@solana/web3.js");
-
-//import user input functionality
-const prompt = require("prompt-sync")();
-
-const publicKey = prompt("Enter your public key:");
-// const newPair = new Keypair();
-
-// // Extract the public key from the keypair
-// const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
-
-// Connect to the Devnet
 const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+const walletBalance = async (publicKey) => {
+  let balance = await connection.getBalance(new PublicKey(publicKey));
+  return balance;
+};
 
-console.log("Public Key: ", publicKey);
+const transferSol = async () => {
+  // Generate a new keypair
+  const from = Keypair.generate();
 
-// Get the wallet balance from a given private key
-const getWalletBalance = async () => {
-  try {
-    // Get balance of the user provided wallet address
-    const walletBalance = await connection.getBalance(new PublicKey(publicKey));
+  // Generate another Keypair (account we'll be sending to)
+  const to = Keypair.generate();
+  {
+    let from_balance = await walletBalance(from.publicKey);
     console.log(
-      `Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`
+      `From wallet balance: ${parseInt(from_balance) / LAMPORTS_PER_SOL}`
     );
-  } catch (err) {
-    console.log(err);
+    let to_balance = await walletBalance(to.publicKey);
+    console.log(
+      `From wallet balance: ${parseInt(to_balance) / LAMPORTS_PER_SOL}`
+    );
+  }
+
+  // Aidrop 2 SOL to Sender wallet
+  console.log("Airdopping some SOL to Sender wallet!");
+  const fromAirDropSignature = await connection.requestAirdrop(
+    new PublicKey(from.publicKey),
+    2 * LAMPORTS_PER_SOL
+  );
+
+  // Latest blockhash (unique identifer of the block) of the cluster
+  let latestBlockHash = await connection.getLatestBlockhash();
+
+  // Confirm transaction using the last valid block height (refers to its time)
+  // to check for transaction expiration
+  await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: fromAirDropSignature,
+  });
+
+  console.log("Airdrop completed for the Sender account");
+  {
+    let from_balance = await walletBalance(from.publicKey);
+    console.log(
+      `From wallet balance: ${parseInt(from_balance) / LAMPORTS_PER_SOL}`
+    );
+    let to_balance = await walletBalance(to.publicKey);
+    console.log(
+      `From wallet balance: ${parseInt(to_balance) / LAMPORTS_PER_SOL}`
+    );
+  }
+
+  // Send money from "from" wallet and into "to" wallet
+  let transfer_amount = (await walletBalance(from.publicKey)) / 2;
+  var transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: from.publicKey,
+      toPubkey: to.publicKey,
+      lamports: transfer_amount,
+    })
+  );
+
+  // Sign transaction
+  var signature = await sendAndConfirmTransaction(connection, transaction, [
+    from,
+  ]);
+  console.log("Signature is", signature);
+  {
+    let from_balance = await walletBalance(from.publicKey);
+    console.log(
+      `From wallet balance: ${parseInt(from_balance) / LAMPORTS_PER_SOL}`
+    );
+    let to_balance = await walletBalance(to.publicKey);
+    console.log(
+      `From wallet balance: ${parseInt(to_balance) / LAMPORTS_PER_SOL}`
+    );
   }
 };
 
-const airDropSol = async () => {
-  try {
-    // Request airdrop of 2 SOL to the wallet
-    console.log("Airdropping some SOL to the wallet!");
-    const fromAirDropSignature = await connection.requestAirdrop(
-      new PublicKey(publicKey),
-      2 * LAMPORTS_PER_SOL
-    );
-    await connection.confirmTransaction(fromAirDropSignature);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-// Show the wallet balance before and after airdropping SOL
-const mainFunction = async () => {
-  await getWalletBalance();
-  await airDropSol();
-  await getWalletBalance();
-};
-
-mainFunction();
+transferSol();
